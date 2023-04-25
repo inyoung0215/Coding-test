03eb4b0da985d6f7b494131e1f48b5f98cd64ac1
youngjun <sohn919@naver.com>
2023-04-25 12:08:16 +0900

    week2 손영준

---
+++ src/week1/SonYoungJun/q1/Main.java
@@ -0,0 +1,13 @@
+package week1.SonYoungJun.q1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(new StringTokenizer(br.readLine()).countTokens());
+    }
+}
