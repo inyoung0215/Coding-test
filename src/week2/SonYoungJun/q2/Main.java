c63fea61b719a997c5bff6b6262294d6bc6be4dc
youngjun <sohn919@naver.com>
2023-05-03 16:53:19 +0900

    week2 손영준

---
+++ src/week2/SonYoungJun/q2/Main.java
@@ -0,0 +1,43 @@
+package week2.SonYoungJun.q2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Point implements Comparable<Point>{
+    int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x == o.x)
+            return this.y - o.y;
+        return this.x - o.x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Point> arr = new ArrayList<>();
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(Point o : arr) {
+            sb.append(o.x + " " + o.y + "\n");
+        }
+        System.out.print(sb);
+    }
+}
---
+++ src/week2/SonYoungJun/q4/Main.java
@@ -0,0 +1,37 @@
+package week2.SonYoungJun.q4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int X = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        int lt = 0, rt = arr.length - 1;
+        int answer = 0;
+        while(lt < rt) {
+            int sum = arr[lt] + arr[rt];
+            if(sum > X) {
+                rt--;
+                continue;
+            }
+            if(sum < X) {
+                lt++;
+                continue;
+            }
+            answer++;
+            lt++;
+        }
+        System.out.print(answer);
+    }
+}
