c9e3f0c303be2eb4c5b2a4ebe3cd8ab255acea46
kimkyungjun <kyungjun1129@naver.com>
2023-05-11 12:22:22 +0900

    week3 김경준

--- src/week3/kyungjunkim/q3/Main.java
+++ src/week3/kyungjunkim/q3/Main.java
@@ -3,19 +3,17 @@ package week3.kyungjunkim.q3;
 import java.io.*;
 import java.util.StringTokenizer;
 
-import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int ComputerNum;
-    static int NetworkNum;
+    static int N;
+    static int M;
     static boolean[][] graph;
     static boolean[] visit;
     static int count;
     static void dfs(int v){
         count++;
         visit[v] = true;
-        for(int i=1;i<=ComputerNum;i++){
+        for(int i=1;i<=N;i++){
             if((!visit[i])&&(graph[v][i])){
                 dfs(i);
             }
@@ -24,11 +22,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ComputerNum = Integer.parseInt(br.readLine());
-        NetworkNum = Integer.parseInt(br.readLine());
-        graph = new boolean[ComputerNum+1][ ComputerNum+1];
-        visit = new boolean[ComputerNum+1];
-        for(int i=0; i<NetworkNum;i++){
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        graph = new boolean[N+1][ N+1];
+        visit = new boolean[N+1];
+        for(int i=0; i<M;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a1 = Integer.parseInt(st.nextToken());
             int a2 = Integer.parseInt(st.nextToken());
