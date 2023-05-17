8768dc049184ac80d46be6527922f47dc8153593
inyoung <charmingyear@gmail.com>
2023-05-17 23:45:16 +0900

    week4 : 4주차 1차코드 제출

---
+++ src/week4/HwangInyoung/Q1.java
@@ -0,0 +1,43 @@
+package week4.HwangInyoung;
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] arr1 = new int[n][m];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                arr1[i][j] = scanner.nextInt();
+            }
+        }
+        int l = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[][] arr2 = new int[l][k];
+        for (int i = 0; i < l; i++){
+            for (int j = 0; j < k; j++){
+                arr2[i][j] = scanner.nextInt();
+            }
+        }
+
+        int[][] result = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                for (int z = 0; z < m; z++) {
+                    result[i][j] += arr1[i][z] * arr2[z][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
