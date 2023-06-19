0da0eb95a7a90cab95a5074b2304a89d3225fa74
inyoung <charmingyear@gmail.com>
2023-06-19 23:29:49 +0900

    week8 : 1차 황인영

---
+++ src/week8/Q2.java
@@ -0,0 +1,28 @@
+package week8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(dp(n));
+        br.close();
+    }
+
+    private static int dp(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        return dp[n];
+    }
+}
+
