94891bea5a2be61e4df48b40eb697cfa46906e6d
youngjun <sohn919@naver.com>
2023-06-20 12:55:06 +0900

    week8 1차 손영준

---
+++ src/week8/SonYoungJun/q1/Main.java
@@ -0,0 +1,26 @@
+package week8.SonYoungJun.q1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        int[] dp = new int[N + 1];
+        for(int i=1; i<=N; i++) arr[i] = Integer.parseInt(br.readLine());
+        for(int i=1; i<=N; i++) {
+            if(i == 1) {
+                dp[1] = arr[1];
+                continue;
+            } else if(i == 2) {
+                dp[2] = arr[1] + arr[2];
+                continue;
+            }
+            dp[i] = Math.max(dp[i-3] + arr[i-1], dp[i-2]) + arr[i];
+        }
+        System.out.print(dp[N]);
+    }
+}
